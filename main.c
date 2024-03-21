e97ad7c735f112a9cf865edd7ed99263e7c03abc
Ermanno di Giulio <digiulioermanno@gmail.com>
2024-03-21 10:22:40 +0100

    Créé les sockets

--- main.c
+++ main.c
@@ -1,7 +1,15 @@
-// ./steps/step000.c
+#include <errno.h>
 #include <stdio.h>
+#include <sys/socket.h>
 
 int main() {
-  printf("hello, world\n");
+  // create a socket
+  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sockfd == -1) {
+    perror("webserver (socket)");
+    return 1;
+  }
+  printf("socket created successfully\n");
+
   return 0;
 }
