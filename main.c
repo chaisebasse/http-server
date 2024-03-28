d806ee55ae7664cfc1c8e40e48d4c0475c5b229f
Ermanno di Giulio <digiulioermanno@gmail.com>
2024-03-28 19:09:54 +0100

    Ajouté des commentaires pour compréhension du code

--- main.c
+++ main.c
@@ -6,27 +6,37 @@
 #define PORT 8080
 
 int main() {
-    // Création du socket
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd == -1) {
-        perror("webserver (socket)");
-        return 1;
-    }
-    printf("socket created successfully\n");
+  // Création du socket
 
-    struct sockaddr_in host_addr;
-    int host_addrlen = sizeof(host_addr);
+  // Paramètres de la fonction : famille d'adresses, type de socket, protocole ip
+  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-    host_addr.sin_family = AF_INET;
-    host_addr.sin_port = htons(PORT);
-    host_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  if (sockfd == -1) {
+    // afficher ce message et le message d'erreur du système avec perror
+    perror("webserver (socket)");
 
-    // Bind le socket à l'addresse
-    if (bind(sockfd, (struct sockaddr *)&host_addr, host_addrlen) != 0) {
-        perror("webserver (bind)");
-        return 1;
-    }
-    printf("socket successfully bound to address\n");
+    return 1;
+  }
+  printf("socket created successfully\n");
 
-    return 0;
+  // Bind le socket à l'addresse
+
+  // Déclaration et initialisation de l'adresse
+  struct sockaddr_in host_addr;
+  host_addr.sin_family = AF_INET;
+  // Convertir l'ordre des octets de l'hôte à l'ordre des octets du réseau
+  host_addr.sin_port = htons(PORT);
+  // Conversion de l'ordre des octets 1a celui du réseau recommandée par man 7 ip mais pas obligatoire
+  host_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+  int host_addrlen = sizeof(host_addr);
+
+  // Paramètres de la fonction : adresse choisie convertie en type accepté par la fonction, taille de l'adresse
+  if (bind(sockfd, (struct sockaddr *)&host_addr, host_addrlen) != 0) {
+    perror("webserver (bind)");
+    return 1;
+  }
+  printf("socket successfully bound to address\n");
+
+  return 0;
 }
