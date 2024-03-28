82406411ddec2027215bd92120a8052af68ac0fa
Ermanno di Giulio <digiulioermanno@gmail.com>
2024-03-28 18:49:39 +0100

    Lié le socket à l'adresse

--- main.c
+++ main.c
@@ -1,15 +1,32 @@
+#include <arpa/inet.h>
 #include <errno.h>
 #include <stdio.h>
 #include <sys/socket.h>
 
-int main() {
-  // create a socket
-  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  if (sockfd == -1) {
-    perror("webserver (socket)");
-    return 1;
-  }
-  printf("socket created successfully\n");
+#define PORT 8080
 
-  return 0;
+int main() {
+    // Création du socket
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        perror("webserver (socket)");
+        return 1;
+    }
+    printf("socket created successfully\n");
+
+    struct sockaddr_in host_addr;
+    int host_addrlen = sizeof(host_addr);
+
+    host_addr.sin_family = AF_INET;
+    host_addr.sin_port = htons(PORT);
+    host_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    // Bind le socket à l'addresse
+    if (bind(sockfd, (struct sockaddr *)&host_addr, host_addrlen) != 0) {
+        perror("webserver (bind)");
+        return 1;
+    }
+    printf("socket successfully bound to address\n");
+
+    return 0;
 }
